cf2dc60137ad9f3e8632b371322716e358d2fa94
luczak20p <91263539+luczak20p@users.noreply.github.com>
2022-05-12 10:26:15 +0200

    Add files via upload

--- testuje.php
+++ testuje.php
@@ -1,3 +1,15 @@
+<?php 
+ob_start();
+session_start();
+$plikiWyniki = array();
+$pliki = scandir("pliki/{$_SESSION["user"]}");
+    foreach ($pliki as $key => $value) {
+        if (!in_array($value, array(".", ".."))) {
+            $plikiWyniki[$key] = $pliki[$key];
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +21,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body class='bg-dark bg-gradient text-light'>
 
     <form action=testuje.php method=post class="m-3">
         <div class="mb-3">
@@ -19,6 +31,28 @@
             <label for=wiado class="form-label">Zawartość pliku<textarea name=wiado id=wiado class="form-control"></textarea></label>
         </div>
         <input type=submit value="Utwórz plik" class="btn btn-success">
+
+    </form>
+
+    <form action=testuje.php method=post class="m-3">
+        <div class="mb-3">
+            <label for=tytul3 class="form-label">Tytuł pliku
+            <select name=tytul3 id=tytul3 class="form-select">
+            <?php 
+            foreach($plikiWyniki as $key=>$value){
+                echo "<option class='ms-3'>{$plikiWyniki[$key]}</option>";
+            }
+            
+            ?>
+            </select>
+            </label>
+            <button>Wybierz plik</button>
+
+        </div>
+        <div class="mb-3">
+            <label for=wiado2 class="form-label">Zawartość pliku<textarea name=wiado2 id=wiado2 class="form-control"></textarea></label>
+        </div>
+        <input type=submit value="Edytuj plik" class="btn btn-primary">
 
     </form>
 
@@ -35,8 +69,7 @@
 </html>
 
 <?php
-ob_start();
-session_start();
+
 $_SESSION["ErrorMassage"] = "Błędne hasło lub nazwa użytkownika";
 
 //logowanie do osobnego
@@ -58,35 +91,30 @@ $aaaaaaaaaa = file_get_contents("uzytkownicy.txt");
 $c = array_filter(explode(",", $aaaaaaaaaa));
 var_dump($c);
 
-$plikiWyniki = array();
-
 if (isset($_POST["wiado"]) && isset($_POST["tytul"]) && preg_match('/^([A-Za-z0-9-_]+)$/', $_POST["tytul"])) {
     fwrite(fopen("pliki/{$_SESSION["user"]}/{$_POST["tytul"]}.txt", "w+"), "{$_POST["wiado"]}");
-    $pliki = scandir("pliki/{$_SESSION["user"]}");
-    foreach ($pliki as $key => $value) {
-        if (!in_array($value, array(".", ".."))) {
-            $plikiWyniki[$key] = $pliki[$key];
-        }
-    }
-    echo "<div class='bg-dark bg-gradient text-light'><h2 class='ms-3'>Pliki użytkownika:</h2>";
-    foreach($plikiWyniki as $key=>$value){
-        echo "<h3 class='ms-3'>{$plikiWyniki[$key]}</h3>";
-    }
-    echo "</div>";
+    header("Refresh:0");
     
 }
+
+
+
+echo "<div><h2 class='ms-3'>Pliki użytkownika:</h2>";
+foreach($plikiWyniki as $key=>$value){
+    echo "<h3 class='ms-3'>{$plikiWyniki[$key]}</h3>";
+}
+echo "</div>";
 
 if (isset($_POST["usun"]) && preg_match('/^([A-Za-z0-9-_]+)$/', $_POST["usun"])) {
     if (is_writable("pliki/{$_SESSION["user"]}/{$_POST["usun"]}.txt")) {
         unlink("pliki/{$_SESSION["user"]}/{$_POST["usun"]}.txt");
-        echo "plik został usunięty";
         $pliki = scandir("pliki/{$_SESSION["user"]}");
         foreach ($pliki as $key => $value) {
             if (!in_array($value, array(".", ".."))) {
                 $plikiWyniki[$key] = $pliki[$key];
             }
         }
-        print_r($plikiWyniki);
+        header("Refresh:0");
     } else {
         echo "nie ma takiego pliku";
     }
